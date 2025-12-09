ee168ad7b20d547d39f6dc5bc8595a4ceb5c1e78
Akshath Sivachidhambaram <akshath.siva@gmail.com>
2025-12-09 17:38:41 -0500

    refined front-end and began implementing routing

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 from static import webscraping
 
 app = Flask(__name__)
+app.secret_key = ""
 @app.route('/')
-def rendered():
+def home():
     articles = webscraping.scrape()
     return render_template('index.html', articles=articles)
+
+@app.route('/saved')
+def saved():
+    pass    
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/callback')
+def callback():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
